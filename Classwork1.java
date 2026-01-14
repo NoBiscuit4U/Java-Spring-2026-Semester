51e71c42d4d2ea6bb29c2027b1b446b0d2985e50
Daniel Bragg <114534607+NoBiscuit4U@users.noreply.github.com>
2026-01-14 09:24:59 -0600

    Inital Commit

---
+++ Classwork1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Classwork1 {
+	public static void main(String[] args) {
+		Scanner scnr=new Scanner(System.in);
+		
+		int wage=scnr.nextInt();
+		
+		System.out.print("Salary is ");
+		System.out.println(wage*40*52);
+	}
+}
